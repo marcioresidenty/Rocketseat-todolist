31e2d3517e1a6545513dbad16d3c3d953c3b9cf7
Marcio Nogueira da Silva <1310227@inca.local>
2023-10-09 14:38:33 -0300

    Aula 01 - Construção back-end de uma aplicação de To-Do List

--- src/main/java/br/com/rocketseat/todolist/TodolistApplication.java
+++ src/main/java/br/com/rocketseat/todolist/TodolistApplication.java
@@ -3,7 +3,7 @@ package br.com.rocketseat.todolist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class TodolistApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/com/rocketseat/todolist/user/UserModel.java
@@ -0,0 +1,10 @@
+package br.com.rocketseat.todolist.user;
+
+public class UserModel {
+    
+   private String username;
+   private String name;
+   private String password;
+
+
+}
