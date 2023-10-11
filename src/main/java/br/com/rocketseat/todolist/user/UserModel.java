578a52f985d23bdcb7a9ef1c988a8e07372a60c0
Marcio Nogueira da Silva <1310227@inca.local>
2023-10-11 16:31:38 -0300

    aula 02 - Integração com Banco de dados

--- src/main/java/br/com/rocketseat/todolist/user/UserModel.java
+++ src/main/java/br/com/rocketseat/todolist/user/UserModel.java
@@ -1,10 +1,32 @@
 package br.com.rocketseat.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+
+@Data   //anotação LOMBOK que carrega os getters e setters
+
+@Entity(name = "tb_users")
 public class UserModel {
-    
+
+   @Id
+   @GeneratedValue(generator = "UUID")
+   private UUID id;
+
+   @Column(unique = true) // não permite cadastro iguais 
    private String username;
    private String name;
    private String password;
 
+   @CreationTimestamp
+   private LocalDateTime createdAt;
 
 }
